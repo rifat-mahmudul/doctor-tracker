1ff3e3ac67f972bff0f825d208e0d86658d43c7d
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-04-28 12:09:36 +0600

    doctors get api done

--- src/app/api/doctors/route.ts
+++ src/app/api/doctors/route.ts
@@ -38,3 +38,50 @@ export async function POST(req: NextRequest) {
     );
   }
 }
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const { searchParams } = new URL(req.url);
+
+    const search = searchParams.get("search") || "";
+    const page = parseInt(searchParams.get("page") || "1");
+    const limit = parseInt(searchParams.get("limit") || "10");
+
+    const skip = (page - 1) * limit;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const query: any = {};
+
+    if (search) {
+      query.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { hospital: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    const doctors = await Doctor.find(query)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const total = await Doctor.countDocuments(query);
+
+    return NextResponse.json({
+      success: true,
+      data: doctors,
+      pagination: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    });
+  } catch (error) {
+    console.log("error from get doctor: ", error);
+    return NextResponse.json(
+      { message: "Failed to fetch doctors" },
+      { status: 500 },
+    );
+  }
+}
